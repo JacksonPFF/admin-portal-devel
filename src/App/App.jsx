ecaa7b3285fe6fd2e89bdaee0d98e5cedbfbda6f
JacksonPFF <jackson.howell@piaggiofastforward.com>
2020-03-18 19:56:15 -0400

    refactor app component from class to functional, update packages

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { history } from '../_helpers';
@@ -7,42 +7,37 @@ import { PrivateRoute } from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+function App(props) {
+  const { dispatch } = props;
+  const { alert } = props;
+  
+  history.listen((location, action) => {
+    // clear alert on location change
+    dispatch(alertActions.clear());
+  });
 
-    const { dispatch } = this.props;
-    history.listen((location, action) => {
-      // clear alert on location change
-      dispatch(alertActions.clear());
-    });
-  }
+  return (
+    <div className="jumbotron">
+      <div className="container">
 
-  render() {
-    const { alert } = this.props;
-    return (
-      <div className="jumbotron">
-        <div className="container">
-
-          <div className="row">
-            <div className="col-md-4 offset-md-4">
-              {alert.message &&
-                <div className={`alert   ${alert.type}`}>{alert.message}</div>
-              }
-            </div>
+        <div className="row">
+          <div className="col-md-4 offset-md-4">
+            {alert.message &&
+              <div className={`alert   ${alert.type}`}>{alert.message}</div>
+            }
           </div>
-
-          <Router history={history}>
-          <div className="row">
-              <PrivateRoute exact path="/" component={HomePage} />
-              
-              <Route path="/login" component={LoginPage} />
-          </div>
-          </Router>
         </div>
+
+        <Router history={history}>
+        <div className="row">
+            <PrivateRoute exact path="/" component={HomePage} />
+            
+            <Route path="/login" component={LoginPage} />
+        </div>
+        </Router>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
