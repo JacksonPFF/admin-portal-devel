f24d2f1dcdf05f692a26278cb88b8c271dc9dc85
JacksonPFF <jackson.howell@piaggiofastforward.com>
2020-03-19 13:50:56 -0400

    refactor HomePage from class to functional

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,49 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { userActions } from '../_actions';
 
-class HomePage extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(userActions.getAll());
-    }
+function HomePage(props) {
+  const { user, users, dispatch } = props;
 
-    render() {
-        const { user, users } = this.props;
-        return (
-            <div className="col-md-6 offset-md-3">
-                <img src={user.profilePicture} 
-                    className="
-                        img-fluid 
-                        rounded-circle
-                        w-25
-                        mb-3" 
-                    alt="Profile Picture"></img>
-                <h1>Hi {user.firstName}!</h1>
-                <p>You're logged in with React & JWT!!</p>
-                <h3>Registered Gitas:</h3>
-                <p>From secure api endpoint</p>
-                {users.loading && <em>Loading users...</em>}
-                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-                {users.items &&
-                    <ul>
-                        {users.items.map((user, index) =>
-                            <li key={user.serial} className="mb-3">
-                                <div>{user.output.serial}</div> 
-                                <div>{user.output.className}</div>
-                                <div>{user.output.registeredByUsername}</div>
-                                <div>{user.output.registeredByEmail}</div>
-                                <div>{user.output.created}</div>
-                            </li>
-                        )}
-                    </ul>
-                }
-                <p>
-                    <Link to="/login">Logout</Link>
-                </p>
-            </div>
-        );
-    }
+  useEffect(() => {
+    dispatch(userActions.getAll());
+  }, []);
+
+  return (
+    <div className="col-md-6 offset-md-3">
+        <img src={user.profilePicture} 
+            className="
+                img-fluid 
+                rounded-circle
+                w-25
+                mb-3" 
+            alt="Profile Picture"></img>
+        <h1>Hi {user.firstName}!</h1>
+        <p>You're logged in with React & JWT!!</p>
+        <h3>Registered Gitas:</h3>
+        <p>From secure api endpoint</p>
+        {users.loading && <em>Loading users...</em>}
+        {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+        {users.items &&
+          <ul>
+            {users.items.map((user, index) =>
+              <li key={user.serial} className="mb-3">
+                <div>{user.output.serial}</div> 
+                <div>{user.output.className}</div>
+                <div>{user.output.registeredByUsername}</div>
+                <div>{user.output.registeredByEmail}</div>
+                <div>{user.output.created}</div>
+              </li>
+            )}
+          </ul>
+        }
+        <p>
+            <Link to="/login">Logout</Link>
+        </p>
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
