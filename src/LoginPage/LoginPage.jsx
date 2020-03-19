6db51419e94012bf32f8d67d6473514d57827711
JacksonPFF <jackson.howell@piaggiofastforward.com>
2020-03-19 12:24:21 -0400

    first pass at converting LoginPage from class to functional

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,69 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { userActions } from '../_actions';
 
-class LoginPage extends React.Component {
-  constructor(props) {
-    super(props);
+function LoginPage(props) {
+  // constructor(props) {
+  //   super(props);
 
-    // reset login status
-    this.props.dispatch(userActions.logout());
+  //   // reset login status
+  //   this.props.dispatch(userActions.logout());
 
-    this.state = {
-      email: '',
-      password: '',
-      submitted: false
-    };
+  //   this.state = {
+  //     email: '',
+  //     password: '',
+  //     submitted: false
+  //   };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
 
-  handleChange(e) {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
+  // const handleChange(e) {
+  //   const { name, value } = e.target;
+  //   setState({ [name]: value });
+  // }
 
-  handleSubmit(e) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleSubmit(e) {
     e.preventDefault();
     event.target.className += " was-validated";
 
-    this.setState({ submitted: true });
-    const { email, password } = this.state;
-    const { dispatch } = this.props;
+    setIsSubmitted(!isSubmitted);
+    //const { email, password } = state;
+    const { dispatch } = props;
+
     if (email && password) {
       dispatch(userActions.login(email, password));
     }
   }
 
-  render() {
-    const { loggingIn } = this.props;
-    const { email, password, submitted } = this.state;
-    return (
-      <div className="col-md-4 offset-md-4">
-        <h2>Login</h2>
-        <form name="form" className="needs-validation" onSubmit={this.handleSubmit} noValidate>
-          <div className={'form-group' + (submitted && !email ? ' has-danger' : '')}>
-            <label htmlFor="email">Email or Username</label>
-            <input type="text" className="form-control" name="email" required value={email} onChange={this.handleChange} />
-            <div className="invalid-feedback">Email is required</div>
-          </div>
-          <div className={'form-group' + (submitted && !password ? ' has-danger' : '')}>
-            <label htmlFor="password">Password</label>
-            <input type="password" className="form-control" name="password" required value={password} onChange={this.handleChange} />
-            <div className="invalid-feedback">Password is required</div>
-          </div>
-          <div className="form-group">
-            <button className="btn btn-primary">Login</button>
-            {loggingIn &&
-              <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-            }
-          </div>
-        </form>
-      </div>
-    );
-  }
+  const { loggingIn } = props;
+  // const { email, password, isSubmitted } = state;
+
+  return (
+    <div className="col-md-4 offset-md-4">
+      <h2>Login</h2>
+      <form name="form" className="needs-validation" onSubmit={handleSubmit} noValidate>
+        <div className={'form-group' + (isSubmitted && !email ? ' has-danger' : '')}>
+          <label htmlFor="email">Email or Username</label>
+          <input type="text" className="form-control" name="email" required value={email} onChange={e => setEmail(e.target.value)} />
+          <div className="invalid-feedback">Email is required</div>
+        </div>
+        <div className={'form-group' + (isSubmitted && !password ? ' has-danger' : '')}>
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" name="password" required value={password} onChange={e => setPassword(e.target.value)} />
+          <div className="invalid-feedback">Password is required</div>
+        </div>
+        <div className="form-group">
+          <button className="btn btn-primary">Login</button>
+          {loggingIn &&
+            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+          }
+        </div>
+      </form>
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
