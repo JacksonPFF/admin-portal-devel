787fa30caed8bd5a9e4f15923b7b97b4309b8dac
JacksonPFF <jackson.howell@piaggiofastforward.com>
2020-03-25 11:22:04 -0400

    got the search filter demo working

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -5,40 +5,82 @@ import { skipCodeConstants } from '../_constants';
 
 function HomePage(props) {
   const { user, registeredGitas, skipCode, dispatch } = props;
+  const [items, setItems] = useState([]);
+
+  const items_data = [
+    'apple',
+    'computer',
+    'windows',
+    'mouse',
+    'keyboard',
+    'monitor',
+    'smartphone'
+  ];
 
   useEffect(() => {
     dispatch(registeredGitasActions.getAll());
+    setItems(items_data);
+
     dispatch({ type: skipCodeConstants.SKIPCODE_POLL_START });
     return function cleanup() {
       dispatch({ type: skipCodeConstants.SKIPCODE_POLL_STOP });
     };
   }, []);
 
+  function handleSearchInput(e) {
+    var searchText = e.target.value;
+    
+    var filteredItems = items_data.filter(function(item) { // this.props.items.
+        return item.search(searchText) > -1;
+    });
+    
+    setItems(filteredItems);
+  }
+
   return (
     <div className="row">
-      <div className="col-md-6">
+      <div className="col-md-3">
         <div className="sticky-top">
           <h4>Registered Gitas:</h4>
           <p>From secure api endpoint</p>
+          
+          <input className="filter form-control 
+                            mb-3" 
+            onInput={handleSearchInput} 
+            type="text" 
+            placeholder="Search for..."/>
+
           {registeredGitas.loading && <em>Loading registered gitas...</em>}
           {registeredGitas.error && <span className="text-danger">ERROR: {registeredGitas.error}</span>}
           {registeredGitas.items &&
-            <ul>
-              {registeredGitas.items.map((gita, index) =>
-                <li key={gita.serial} className="mb-3">
-                  <div><b>Serial:</b> {gita.output.serial}</div> 
-                  <div><b>Name:</b> {gita.output.name}</div>
-                  <div><b>Registered by:</b> {gita.output.registeredByUsername}</div>
-                  <div><b>Email:</b> {gita.output.registeredByEmail}</div>
-                  <div><b>Date:</b> {gita.output.created}</div>
-                </li>
-              )}
-            </ul>
+            <div>
+              <ul className="filtered-stuff">
+                {items.map((item, index) => // this.state.items.
+                  <li key={index} className="list-group-item">
+                    {`${item}`}
+                  </li>
+                )}
+              </ul>
+              
+              <ul className="registered-gitas">
+                {registeredGitas.items.map((gita, index) =>
+                  <li key={gita.serial} className="mb-3">
+                    <div><b>Serial:</b> {gita.output.serial}</div> 
+                    <div><b>Name:</b> {gita.output.name}</div>
+                    <div><b>Registered by:</b> {gita.output.registeredByUsername}</div>
+                    <div><b>Email:</b> {gita.output.registeredByEmail}</div>
+                    <div><b>Date:</b> {gita.output.created}</div>
+                  </li>
+                )}
+              </ul>
+            </div>
           }
         </div>
       </div>
+      
+      <div className="col-md-3"></div>
 
-      <div className="col-md-6">
+      <div className="col-md-3">
         <h4> Wifi Skip Code </h4>
         <p>Updates every 15 seconds</p>
         <h1><b>{ skipCode.data }</b></h1>
--- src/_components/SidebarLayout.jsx
+++ src/_components/SidebarLayout.jsx
@@ -83,7 +83,7 @@ function SidebarLayout(props) {
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
               <h1 className="h2 mt-3">Dashboard</h1>
               <div className="btn-toolbar mb-2 mb-md-0">
-                <div className="btn-group mr-2">
+                <div className="btn-group">
                   <button className="btn btn-sm btn-outline-secondary">Share</button>
                   <button className="btn btn-sm btn-outline-secondary">Export</button>
                 </div>
